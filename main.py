e91828256774ccff179e79392cdf2115572277e6
Gold-Donut <ayaanmeister@gmail.com>
2026-03-30 19:08:46 -0400

    added stronger updating for the espn api data, now the timing of the games is considered to see if it should be displayed

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from data import espn
 # TEST 1
 # this test is to see if I am properly getting the data for a single sport
 testSport = "basketball"
-testLeague = "womens-college-basketball"
+testLeague = "nba"
+api = espn.getJSON(espn.apiEndpoint(testSport, testLeague))
 
-games = espn.getScoreboard(testSport, testLeague)
-for game in games:
+games = espn.getScoreboardList(testSport, testLeague)
+
+freshGames = espn.refreshGameList(games, espn.getJSON(espn.apiEndpoint(testSport, testLeague)), testSport)
+
+for game in freshGames.values():
     print(game)
+
+print("done")
