fab38338d992ba97fd5b46c6c6d5c0a6fac899dc
Gold-Donut <ayaanmeister@gmail.com>
2026-04-13 20:31:02 -0400

    import bug fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .renderer import Renderer
 from .config_loader import loadConfig
 from .layout import drawFont, drawImage
-from .overrides import getDisplayAbbr, LOGO_OVERRIDES
+from .overrides import getDisplayAbbr
 from .time_utils import parseDate, formatLocalTime
 from .brightness import getBrightness
