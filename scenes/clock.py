394045c4f08a2c26c6f4e13df1ddaa9d7c6aaa36
Gold-Donut <ayaanmeister@gmail.com>
2026-06-22 19:54:07 -0400

    fixed default clock formatting bug

--- scenes/clock.py
+++ scenes/clock.py
@@ -5,5 +5,5 @@ class ClockScene(BaseScene):
     def __init__(self):
         super().__init__()
         self.drawings = {
-            "clock" : FontArgs(JERSEY20_FONT, str(time.strftime("%I:%M")), 64, 16, white)
+            "clock" : FontArgs(JERSEY20_FONT, time.strftime("%I:%M %p").lstrip("0"), 64, 16, white)
         }
